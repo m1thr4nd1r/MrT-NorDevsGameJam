4ed604406631b6d8d50ee5614ddcefe199527a48
m1thr4nd1r <vitocr_santos@hotmail.com>
2016-07-23 11:39:04 -0300

    Pre-pre-pre alpha pesadão

---
+++ TempoAoCubo/Assets/LevelGen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGen : MonoBehaviour {
+
+    GameObject player, lastPlatform, template, newPlatform;
+
+    float fix;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        lastPlatform = newPlatform = null;
+        template = GameObject.FindGameObjectWithTag("Platform");
+        //InvokeRepeating("platGen", 0, 5);
+        InvokeRepeating("IncreaseCamera", 0.05f, 0.05f);
+        fix = 0;
+    }
+
+    void IncreaseCamera()
+    {
+        Camera.main.gameObject.transform.position += Vector3.up * 0.05f;
+    }
+	
+    void platGen()
+    {
+        newPlatform = Instantiate<GameObject>(template);
+        if (lastPlatform != null)
+            fix = lastPlatform.transform.position.x > Camera.main.pixelWidth / 2 ? 0.5f : -0.5f;
+        newPlatform.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Mathf.Clamp(Random.value + fix, 0, 1), 1));//Camera.main.ViewportToWorldPoint(new Vector3(1, 0, Camera.main.nearClipPlane)) + Vector3.right * Random.Range(5, Camera.main.pixelWidth - 5);
+        //newPlatform.transform.position = new Vector3(newPlatform.transform.position.x, 2 + (lastPlatform != null ? lastPlatform.transform.position.y : player.transform.position.y), 0);
+        lastPlatform = newPlatform;
+    }
+}
---
+++ TempoAoCubo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    private bool grounded;
+    public float maxSpeed = 5f;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        grounded = false;
+    }
+
+    void FixedUpdate()
+    {
+        print(grounded);
+        //Vector2 v = Vector2.right * maxSpeed * Input.GetAxis("Horizontal");
+        body.velocity = Vector2.right * maxSpeed * Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+            body.AddForce(Vector2.up * 250f , ForceMode2D.Impulse);
+            //body.velocity += 250f * Vector2.up;
+    }
+
+    void die()
+    {
+        Time.timeScale = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.collider.name.Equals("Floor") ||
+            c.collider.name.Equals("Platform"))
+            grounded = true;
+        else if (c.collider.name.Contains("Wall"))
+            die();
+    }
+
+    void OnCollisionExit2D(Collision2D c)
+    {
+        if (c.collider.name.Equals("Floor") ||
+            c.collider.name.Equals("Platform"))
+            grounded = false;
+    }
+}
